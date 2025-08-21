0cb262062c0ca53e3fc840504828b32355c8f580
mllrosa <mllr8sa@gmail.com>
2025-08-21 14:52:02 -0300

    AA

--- json/main.py
+++ json/main.py
@@ -8,5 +8,3 @@ json_dados = {
 #with open("json_dados.json", "w", encoding="utf-8") as arquivo:
 with open ("json_dados", "w", encoding="utf-8") as arquivo:
     json.dump(json_dados, arquivo, ensure_ascii=False,indent=4)
-
-
